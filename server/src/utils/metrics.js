29bff410d5068b1a2ac174295c4dacabb82bad78
Sujal-Raghuvanshi <raghuvanshisujal7@gmail.com>
2026-04-08 13:47:56 +0530

    Revert "Merge pull request #2 from Utkarsh07saraswat/utkarsh"
    This reverts commit e7e5e26a9061f94fe8179012439b0f92c573116a, reversing
    changes made to 2dd0524af798415985955a1456debe3dee3fd305.

--- server/src/utils/metrics.js
+++
@@ -1,9 +0,0 @@
-const { MetricsCollector } = require('./metricsCollector');
-
-// Re-export or extend MetricsCollector
-module.exports = {
-    metrics: MetricsCollector,
-    // Add compatible API if needed
-    recordRequest: MetricsCollector.recordHttpRequest,
-    recordError: MetricsCollector.recordError
-};
