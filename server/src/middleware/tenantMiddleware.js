fffcac7a7d096dd468428478d94fd5d727539e69
Utkarsh07saraswat <saraswatutkarsh04@gmail.com>
2026-04-09 14:23:56 +0530

    Delete server directory

--- server/src/middleware/tenantMiddleware.js
+++
@@ -1,6 +0,0 @@
-const { TenantResolver, enforceTenantIsolation } = require('./tenantResolver');
-
-module.exports = {
-    tenantMiddleware: TenantResolver.fromJWT, // The middleware to resolve tenant
-    enforceTenantIsolation // The middleware to enforce isolation
-};
